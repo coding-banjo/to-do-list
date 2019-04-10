4e480442c2d17397dfed8732ef5dd9c230a1fa92
Quinton Fults <quinton.fults.work@gmail.com>
2019-04-09 20:54:31 -0700

    added List item creator

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 const { firebase, firebaseui } = window
 const { firestore, auth } = firebase
-let user
+let user,
+  currentListId
 
 const config = {
   apiKey: 'AIzaSyAFmkDynYd4zYul38bMyDXyPRomy0PmC8U',
@@ -28,40 +29,65 @@ const ui = new firebaseui.auth.AuthUI(auth())
 ui.start('#auth-container', uiConfig)
 const db = firestore()
 
-const nowUser = userPresence => {
-  document.querySelector('#auth-container').style.display = 'none'
-  user = userPresence
+const getLists = _ => {
   db.collection('lists').where('uid', '==', user.uid).get()
     .then(({ docs }) => {
       if (docs) {
         document.querySelector('#currentList').innerHTML = ''
         document.querySelector('#currentListContainer').style.display = 'block'
-        docs.forEach(doc => {
+        docs.forEach((doc, i) => {
           console.log(doc.data())
+          if (!i) {
+            currentListId = doc.data().id
+          }
           doc.data().items.forEach(item => {
             let itemElem = document.createElement('li')
             itemElem.innerHTML = `
-            ${item.name}
+            ${item.value}
             <button>${item.isDone ? 'Done' : 'Not Done'}</button>
             `
-            document.querySelector('#currentListContainer').append(itemElem)
+            document.querySelector('#currentList').append(itemElem)
           })
         })
       }
     })
 }
+const nowUser = userPresence => {
+  document.querySelector('#auth-container').style.display = 'none'
+  user = userPresence
+  getLists()
+}
 
 const createNewList = _ => {
-  const id = db.collection('lists').doc().id
+  currentListId = db.collection('lists').doc().id
+
   db.collection('lists').doc(id).set({
     name: document.querySelector('#newList').value,
     items: [],
     uid: user.uid
   })
+  getLists()
+}
+
+const createNewListItem = _ => {
+  let currentItemId = db.collection('lists').doc().id
+  db.collection('lists').doc(currentListId).update({
+    items: firestore.FieldValue.arrayUnion({
+      id: currentItemId,
+      value: document.querySelector('#newItem').value,
+      isDone: false
+    })
+  })
+  getLists()
 }
 
 document.querySelector('#createNewList').addEventListener('click', e => {
   e.preventDefault()
   createNewList()
 })
+document.querySelector('#createNewListItem').addEventListener('click', e => {
+  e.preventDefault()
+  createNewListItem()
+})
+
 auth().onAuthStateChanged(userPresence => userPresence ? nowUser(userPresence) : null)
